0ee8c0a9b5e96a9fa74ebed690988796065a89a6
JojoKsv <jkossouvi@gmail.com>
2024-11-07 18:36:02 +0000

    fifth commit

--- src/pages/common/Advices.jsx
+++ src/pages/common/Advices.jsx
@@ -4,8 +4,7 @@ import { z } from "zod";
 const inputsFields = [
     ['text', 'Nom et Prénom (s)'],
     ['email', 'Addresse email'],
-    ['text', 'Titre du projet'],
-    ['text', 'Description du projet']
+    ['text', 'Titre du projet']
 ];
 
 // const loginSchema = z.object({
@@ -19,15 +18,15 @@ function Advices(){
     const onSubmit = async (data) => {
         await new Promise((resolver) => setTimeout(resolver, 1000))
         console.log(data);
-    }
+    };
 
-    const renderInputsFields = (type, placeholder) => (
+    const renderInputsFields = (type, placeholder, model) => (
         <div>
             <input
                 type={type}
                 placeholder={placeholder}
                 {...form.register(type)}
-                className="bg-gray-50 border border-purple-700"
+                className="bg-gray-50 border border-indigo-700 placeholder:text-sm placeholder:text-gray-600 pl-4 rounded-md h-10 w-full outline-none focus:ring-4"
             />
             <small>{form.formState.errors[type]?.message}</small>
         </div>
@@ -41,23 +40,32 @@ function Advices(){
                     Donnez vos avis afin de permettre à notre équipe de prendre en compte vos besoin et de vous proposer le meilleur durant les compétitions!
                 </p>
             </div>
-            <div className='flex flex-col items-center gap-4 bg-white flex items-center justify-center rounded-lg font-bold text-xl py-4'>
-                <h1>Formulaire de soumission d'avis</h1>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <div>
-                        {inputsFields.map((fields, index) => (
-                            <div key={index} className="flex gap-4 items-center">
-                                {renderInputsFields(...fields)}
-                            </div>
-                        ))}
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={form.formState.isSubmitting}
-                    >
-                    {form.formState.isSubmitting ? 'Soummition...' : 'Soummetre'}
-                    </button>
-                </form> 
+            <div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2">
+                <div className='flex flex-col flex-1 items-center gap-8 bg-white flex items-center justify-center rounded-lg font-bold text-xl py-8 px-6 shadow-lg '>
+                    <h1 className="underline text-indigo-700 text-2xl">Soumettre un avis</h1>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full">
+                        <div className="flex flex-col gap-4 items-center w-full">
+                            {inputsFields.map((fields, index) => (
+                                <div key={index} className="w-full">
+                                    {renderInputsFields(...fields)}
+                                </div>
+                            ))}
+                            <textarea
+                                rows="7"
+                                placeholder="Description du projet"
+                                className="bg-gray-50 border border-purple-700 text-sm placeholder:text-sm placeholder:text-gray-600 pl-4 pt-4 rounded-md w-full outline-none focus:ring-4"
+                            >
+                            </textarea>
+                        </div>
+                        <button
+                            type="submit"
+                            disabled={form.formState.isSubmitting}
+                            className="bg-indigo-600 text-white h-10 rounded-md"
+                        >
+                            {form.formState.isSubmitting ? 'Soummition...' : 'Soummetre'}
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     )
