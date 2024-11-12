83d04415a6e7b5612cb54d51e0ca8785e0003b5b
JojoKsv <jkossouvi@gmail.com>
2024-11-12 22:03:49 +0000

    Setting again

--- src/components/layouts/common/index.jsx
+++ src/components/layouts/common/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import NavBar from "./NavBar"
-import logo from '../../../assets/images/logo.png'
+import logo from '../../../assets/images/logo1.webp'
 import { PiSealQuestion } from "react-icons/pi";
 import { RxCountdownTimer } from "react-icons/rx";
 import { RiBarChart2Fill } from "react-icons/ri";
@@ -26,10 +26,10 @@ function CommonLayout(){
     return(
         <div className="min-h-screen bg-gray-100">
             <div>
-                <div className="lg:hidden fixed top-8 right-3 z-50 flex items-center gap-2">
+                <div className="lg:hidden fixed top-8 right-3 z-50 flex items-center gap-3">
                     <div className="p-1 cursor-pointer hover:bg-gray-200 rounded-3xl">
                         <GoPerson
-                            size={ 24 }
+                            size={ 26 }
                         />
                     </div>
                     <div className="p-1 cursor-pointer hover:bg-gray-200 rounded-3xl">
@@ -53,7 +53,7 @@ function CommonLayout(){
                 />
                 <Link to="/" className="fixed text-indigo-600 lg:text-gray-300 font-semibold top-4 left-3 lg:bottom-3 lg:top-auto z-20 flex items-center gap-1">
                     <img src={logo} alt="logo" className="h-16" />
-                    <span className="font-extrabold text-2xl">Skillify</span>
+                    {/* <span className="font-extrabold text-2xl">Skillify</span> */}
                 </Link>
                 <main className="lg:pl-64">
                     <div className={`px-4 py-10 pt-36 sm:px-6 lg:px-8`}>
