890c41c84384475e4131903540ed68a4c057e1d3
JojoKsv <jkossouvi@gmail.com>
2024-11-13 22:14:30 +0000

    Home page setting

--- src/pages/common/home.jsx
+++ src/pages/common/home.jsx
@@ -1,5 +1,8 @@
 import { BadgeCheck, Briefcase, Lightbulb, Network } from "lucide-react";
 import { Link } from "react-router-dom";
+import logo from '../../assets/images/logo.png'
+import { VscBriefcase } from "react-icons/vsc";
+import { IoLocationOutline } from "react-icons/io5";
 
 const features = [
   {
@@ -22,9 +25,38 @@ const features = [
     description: 'Profitez d’avantages uniques et faites avancer votre carrière ou entreprise.',
     icon: BadgeCheck,
   },
-  
-  ];
-  
+];
+
+const jobData = [
+  {
+    id: 1,
+    title: "CHARGE DE SUIVI, EVALUATION, REDEVABILITE ET APPRENTISSAGE (MEAL OFFICIER)",
+    organization: "Fond des Nations Unies pour le Developpement",
+    field: "Administration des Affaires - Gestion des Affaires, Gestion, Logistique",
+    location: "ONU, Sénégal, Afrique de l'Ouest",
+    deadline: "17 Novembre 2024",
+    logo: logo,
+  },
+  {
+    id: 2,
+    title: "CHARGEE DE COMMUNICATION",
+    organization: "Organisation des Nations Unies",
+    field: "Communication, Journaliste",
+    location: "ONU, Sénégal, Afrique de l'Ouest",
+    deadline: "12 Decembre 2024",
+    logo: logo,
+  },
+  {
+    id: 3,
+    title: "RESPONSABLE DES OPERATIONS",
+    organization: "Banque Africaine de Développement",
+    field: "Gestion des Opérations, Banque, Finances",
+    location: "BAD, Côte d'Ivoire, Afrique de l'Ouest",
+    deadline: "20 Décembre 2024",
+    logo: logo,
+  }
+];
+
 
 function Home(){
     return(
@@ -42,8 +74,8 @@ function Home(){
               <div className="mt-8 max-w-md mx-auto sm:flex sm:justify-center sm:flex-col md:mt-8">
                 <div className="flex justify-center">
                   <Link
-                    to="challenges"
-                    className="l flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                    to="opportinutes"
+                    className="l flex items-center justify-center px-8 py-3 border text-base font-medium rounded-md text-white hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 button"
                   >
                     Découvrir 
                   </Link>
@@ -72,6 +104,41 @@ function Home(){
                     </div>
                   ))}
                 </dl>
+              </div>
+            </div>
+          </div>
+
+          {/* Offers Section */}
+          <div>
+            <div>
+              <div className="flex flex-col items-center gap-14 mb-14">
+                <div className="w-full flex justify-center">
+                  <h1 className="font-extrabold text-3xl md:text-4xl lg:text-5xl">Nos dernières offres</h1>
+                </div>
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 lg:px-0 md:px-4 px-4">
+                  {jobData.map((job) => (
+                    <div key={job.id} className="border border-gray-300 rounded-2xl px-3 py-4 flex gap-4 hover:border-orange-300">
+                      <img src={job.logo} alt="logo" className="h-10 lg:h-12 md:h-12" />
+                      <div className="flex flex-col gap-1">
+                        <div>
+                          <h1 className="font-semibold lg:text-lg md:text-lg leading-tight">{job.title}</h1>
+                          <span className="font-medium text-gray-400 flex items-center gap-1 text-sm text-orange-500">{job.organization}</span>
+                        </div>
+                        <div className="flex flex-col gap-1 mt-3">
+                          <span className="flex items-center gap-1 text-gray-600">
+                            <VscBriefcase /> {job.field}
+                          </span>
+                          <span className="flex items-center gap-1 text-gray-600">
+                            <IoLocationOutline /> {job.location}
+                          </span>
+                        </div>
+                        <div className="mt-3">
+                          Date limite:<span className="font-bold"> {job.deadline}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
