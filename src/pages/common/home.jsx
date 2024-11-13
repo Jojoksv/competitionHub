2d3bf78e08171e426194100e6bc67f0039da2065
JojoKsv <jkossouvi@gmail.com>
2024-11-13 23:25:36 +0000

    Home page setting

--- src/pages/common/home.jsx
+++ src/pages/common/home.jsx
@@ -267,13 +267,13 @@ function Home(){
           <div className="bg-indigo-50 rounded-xl shadow-inner">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
               <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                <span className="block">Prêt à relever le défi ?</span>
+                <span className="block">Prêt à vous propulser ?</span>
                 <span className="block text-indigo-600">Rejoignez-nous dès aujourd'hui.</span>
               </h2>
               <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                 <div className="inline-flex rounded-md shadow">
                   <Link
-                    to="challenges"
+                    to="opportinutes"
                     className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                   >
                     Commencer
